45595b1d20bad13db93cd8127442b1e56b74c246
SergeyPish <seregapish@gmail.com>
2023-02-28 09:38:57 +0200

    ADD | ADD ORB token with TAX #OB-269

--- contracts/Governance/ORB.sol
+++ contracts/Governance/ORB.sol
@@ -630,15 +630,295 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
     ) internal virtual {}
 }
 
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Wrappers over Solidity's arithmetic operations with added overflow
+ * checks.
+ *
+ * Arithmetic operations in Solidity wrap on overflow. This can easily result
+ * in bugs, because programmers usually assume that an overflow raises an
+ * error, which is the standard behavior in high level programming languages.
+ * `SafeMath` restores this intuition by reverting the transaction when an
+ * operation overflows.
+ *
+ * Using this library instead of the unchecked operations eliminates an entire
+ * class of bugs, so it's recommended to use it always.
+ */
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryAdd(uint256 a, uint256 b)
+        internal
+        pure
+        returns (bool, uint256)
+    {
+        uint256 c = a + b;
+        if (c < a) return (false, 0);
+        return (true, c);
+    }
+
+    /**
+     * @dev Returns the substraction of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function trySub(uint256 a, uint256 b)
+        internal
+        pure
+        returns (bool, uint256)
+    {
+        if (b > a) return (false, 0);
+        return (true, a - b);
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMul(uint256 a, uint256 b)
+        internal
+        pure
+        returns (bool, uint256)
+    {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) return (true, 0);
+        uint256 c = a * b;
+        if (c / a != b) return (false, 0);
+        return (true, c);
+    }
+
+    /**
+     * @dev Returns the division of two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryDiv(uint256 a, uint256 b)
+        internal
+        pure
+        returns (bool, uint256)
+    {
+        if (b == 0) return (false, 0);
+        return (true, a / b);
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMod(uint256 a, uint256 b)
+        internal
+        pure
+        returns (bool, uint256)
+    {
+        if (b == 0) return (false, 0);
+        return (true, a % b);
+    }
+
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        return a - b;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     *
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) return 0;
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "SafeMath: division by zero");
+        return a / b;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "SafeMath: modulo by zero");
+        return a % b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {trySub}.
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        return a - b;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {tryDiv}.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        return a / b;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting with custom message when dividing by zero.
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {tryMod}.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        return a % b;
+    }
+}
+
 // File: contracts/Orbiter/Governance/ORB.sol
 
 pragma solidity 0.8.10;
 
 contract ORBToken is ERC20, Ownable {
     using Address for address;
+    using SafeMath for uint256;
 
-    constructor(uint256 initialSupply) ERC20("ORB token", "ORB") {
-        _mint(msg.sender, initialSupply);
+    mapping(address => bool) public isExcludedFromFee;
+    mapping(address => bool) public isMinter;
+    mapping(address => bool) public isBurner;
+
+    event MinterRoleAssigned(address indexed _user, address indexed _account);
+    event MinterRoleRevoked(address indexed _user, address indexed _account);
+    event BurnerRoleAssigned(address indexed _user, address indexed _account);
+    event BurnerRoleRevoked(address indexed _user, address indexed _account);
+    event FeeUpdated(address indexed _user, uint256 _fee);
+    event FeeWalletUpdated(address indexed _user, address _feeWallet);
+    event TaxStatusUpdated(address indexed _user, bool _status);
+
+    uint256 public SELL_FEE = 2000;
+
+    bool public TAX_ACTIVE = false;
+
+    uint256 public totalTax = 0;
+
+    address public feeWallet;
+
+    modifier hasMinterRole() {
+        require(isMinter[_msgSender()], "ORB: You don't have the permission!");
+        _;
+    }
+
+    modifier hasBurnerRole() {
+        require(isBurner[_msgSender()], "ORB: You don't have the permission!");
+        _;
+    }
+
+    constructor(uint256 _initialSupply, address _feeWallet)
+        ERC20("ORB token", "ORB")
+    {
+        require(_feeWallet != address(0), "ERC20: feeWallet the zero address");
+        feeWallet = _feeWallet;
+        _mint(msg.sender, _initialSupply);
     }
 
     function multisend(address[] memory to, uint256[] memory values)
@@ -659,7 +939,79 @@ contract ORBToken is ERC20, Ownable {
         }
     }
 
-    function burnTokens(uint256 amount) external onlyOwner {
-        _burn(msg.sender, amount);
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal override {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        if (TAX_ACTIVE) {
+            uint256 taxFee = SELL_FEE;
+
+            if (isExcludedFromFee[sender] || isExcludedFromFee[recipient]) {
+                taxFee = 0;
+            }
+            uint256 taxFeeAmount = amount.mul(taxFee).div(10000);
+
+            if (taxFeeAmount > 0) {
+                totalTax = totalTax.add(taxFeeAmount);
+                super._transfer(sender, feeWallet, taxFeeAmount);
+                amount = amount.sub(taxFeeAmount);
+            }
+        }
+
+        super._transfer(sender, recipient, amount);
+    }
+
+    function mint(address _user, uint256 _amount) external hasMinterRole {
+        _mint(_user, _amount);
+    }
+
+    function assignMinterRole(address _account) public onlyOwner {
+        isMinter[_account] = true;
+
+        emit MinterRoleAssigned(_msgSender(), _account);
+    }
+
+    function revokeMinterRole(address _account) public onlyOwner {
+        isMinter[_account] = false;
+
+        emit MinterRoleRevoked(_msgSender(), _account);
+    }
+
+    function burn(address _user, uint256 _amount) external hasBurnerRole {
+        _burn(_user, _amount);
+    }
+
+    function assignBurnerRole(address _account) public onlyOwner {
+        isBurner[_account] = true;
+
+        emit BurnerRoleAssigned(_msgSender(), _account);
+    }
+
+    function revokeBurnerRole(address _account) public onlyOwner {
+        isBurner[_account] = false;
+
+        emit BurnerRoleRevoked(_msgSender(), _account);
+    }
+
+    function updateFee(uint256 _fee) external onlyOwner {
+        SELL_FEE = _fee;
+
+        emit FeeUpdated(_msgSender(), _fee);
+    }
+
+    function updateFeeWallet(address _feeWallet) external onlyOwner {
+        feeWallet = _feeWallet;
+
+        emit FeeWalletUpdated(_msgSender(), _feeWallet);
+    }
+
+    function updateTaxStatus(bool _status) external onlyOwner {
+        TAX_ACTIVE = _status;
+
+        emit TaxStatusUpdated(_msgSender(), _status);
     }
 }
